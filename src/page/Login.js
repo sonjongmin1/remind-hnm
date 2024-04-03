5b6dd064e679a690f3bdb67f69018eb9ef1438d6
jong_min <whdals0525@gmail.com>
2024-04-04 03:14:18 +0900

    second-remind-hnm

--- src/page/Login.js
+++ src/page/Login.js
@@ -25,9 +25,6 @@ const Login = ({ setAuthenticate }) => {
           <Form.Label>Password</Form.Label>
           <Form.Control type='password' placeholder='Password' />
         </Form.Group>
-        <Form.Group className='mb-3' controlId='formBasicCheckbox'>
-          <Form.Check type='checkbox' label='Check me out' />
-        </Form.Group>
         <Button variant='danger' type='submit'>
           로그인
         </Button>
