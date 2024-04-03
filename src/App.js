624f03a92b92944b64acbb8914edf48c22ed01ce
jong_min <whdals0525@gmail.com>
2024-04-04 00:08:24 +0900

    remind-hnm

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetail from './page/ProductDetail';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<ProductAll />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/product/:id' element={<ProductDetail />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+  const menuList = [
+    '여성',
+    'Divided',
+    '남성',
+    '신생아/유아',
+    '아동',
+    'H&M Home',
+    'Sale',
+    '지속가능성',
+  ];
+  return (
+    <div>
+      <div>
+        <div className='login-button'>
+          <FontAwesomeIcon icon={faUser} />
+          <div>로그인</div>
+        </div>
+      </div>
+      <div className='nav-section'>
+        <img
+          width={100}
+          src='https://upload.wikimedia.org/wikipedia/commons/5/53/H%26M-Logo.svg'
+        />
+      </div>
+      <div className='menu-area'>
+        <ul className='menu-list'>
+          {menuList.map((menu) => (
+            <li>{menu}</li>
+          ))}
+        </ul>
+        <div className='search-box'>
+          <FontAwesomeIcon icon={faSearch} />
+          <input type='text' placeholder='제품검색' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
